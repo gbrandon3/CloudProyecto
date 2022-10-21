9b1a68afa17c1e37f04631cfa593b84a09e185a3
Brandon <b.gonzalezp@uniandes.edu.co>
2022-10-20 22:47:27 -0500

    Creacion de container para la aplicacion y postgresql

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ from model import db
 from view import SignUpView, SignInView, TaskView,TasksView
 
 app = Flask(__name__)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@postgres:5432/Cloud'
+                                                
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@postgres:5432/Cloud' # 'postgresql://usuario:contraseña@localhost:5432/Cloud' Cambiar esta linea si no se usa docker
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 app.config['JWT_SECRET_KEY'] = 'frase-secreta'
