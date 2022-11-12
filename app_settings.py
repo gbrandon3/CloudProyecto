064a10d39c410ada40b0c26a7be9cebd558e37fb
Oscar Fernando Alvarez <o.alvareze@uniandes.edu.co>
2022-11-12 17:20:57 -0500

    FIX en la ruta para eliminar el servidor de NFS

--- app_settings.py
+++ app_settings.py
@@ -1,7 +1,7 @@
 #SQLALCHEMY_DATABASE_URI = 'postgresql://cloud:cloud@localhost:5432/cloud' # Cadena de conexión a la base de datos
 SQLALCHEMY_DATABASE_URI = 'postgresql://cloud:cloud@10.27.160.3:5432/cloud' # Cadena de conexión a la base de datos
 #RUTA_REPOSITORIO = "c:/Temp/"
-RUTA_REPOSITORIO = "/RepositorioMISO/"
+RUTA_REPOSITORIO = "/temp/"
 EXTENSIONES_PERMITIDAS = ["mp3","wav", "ogg"]
 EMAIL_SERVER = "datadoc.com.co"
 EMAIL_SERVER_PORT = 587
