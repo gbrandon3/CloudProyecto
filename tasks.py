4ae9bc78b1202bf57a5896ff117f412bfa17baeb
Brandon <b.gonzalezp@uniandes.edu.co>
2022-11-21 15:40:47 -0500

    pub/sub

--- tasks.py
+++ tasks.py
@@ -20,79 +20,81 @@ subscriber = pubsub_v1.SubscriberClient()
 
 sub_path=app_settings.SUB_PATH
 def convert_audio(messageP):
-
+    if messageP.attributes:
+        id=messageP.attributes.get('task_id')
+        pendingTask = session.query(Task).get(int(id))
+        print(pendingTask)
+        if(pendingTask!=None):
+            message = ""
+            valid = True
+            originFileName = pendingTask.file
+            targetExtension = pendingTask.newExtension
+            fileName, extension = os.path.splitext(originFileName)
+            originFilePath = os.path.join(app_settings.RUTA_REPOSITORIO, pendingTask.user, originFileName)
+            targetFilePath = os.path.join(app_settings.RUTA_REPOSITORIO, pendingTask.user, fileName + "." + targetExtension)
     
-    pendingTask = session.query(Task).get(messageP.attributes.get('task_id'))
-    if(pendingTask!=None):
-        message = ""
-        valid = True
-        originFileName = pendingTask.file
-        targetExtension = pendingTask.newExtension
-        fileName, extension = os.path.splitext(originFileName)
-        originFilePath = os.path.join(app_settings.RUTA_REPOSITORIO, pendingTask.user, originFileName)
-        targetFilePath = os.path.join(app_settings.RUTA_REPOSITORIO, pendingTask.user, fileName + "." + targetExtension)
-    
-        if not targetExtension in app_settings.EXTENSIONES_PERMITIDAS:
-            valid = False
-            message = "La extensión del archivo a convertir no se encuentra dentro de las extensiones habilitadas"
-
-        if not targetExtension in app_settings.EXTENSIONES_PERMITIDAS:
-            valid = False
-            message = "La extensión a la que desea convertir no se encuentra dentro de las extensiones habilitadas"
-
-        if "." + targetExtension == extension:
-            valid = False
-            message = "La extensión a la que desea convertir es la misma del archivo orifinal y no se requiere realizar ninguna conversión"
-
-        if valid and not os.path.exists(originFilePath):
-            valid = False
-            message = "El archivo de origen no se encontró"
-
-        if valid and os.path.exists(targetFilePath):
-            valid = False
-            message = "La conversión requerida ya había sido realizada, puede descargar el archivo"
-            validTasks = validTasks + 1
-        print(message)
-        if valid:
-            try:
-                GCPStorage.download_file(pendingTask.user + "/" + pendingTask.file, originFilePath)
-                if extension == ".wav":
-                    input = AudioSegment.from_wav(originFilePath)
-                    input.export(targetFilePath, format=targetExtension)
-                elif extension == ".mp3":
-                    input = AudioSegment.from_mp3(originFilePath)
-                    input.export(targetFilePath, format=targetExtension)
-                elif extension == ".ogg":
-                    input = AudioSegment.from_ogg(originFilePath)
-                    input.export(targetFilePath, format=targetExtension)
-
-                GCPStorage.upload_file(targetFilePath, pendingTask.user + "/" + fileName + "." + targetExtension)
-                message = "El archivo ha sido descargado correctamente, podrá descargarlo con el nombre " + fileName + "." + targetExtension
-                print(message)
-                validTasks = validTasks + 1
-            except Exception as e:
-                traceback.print_exc()
+            if not targetExtension in app_settings.EXTENSIONES_PERMITIDAS:
                 valid = False
-                message = "No se pudo realizar la conversión porque se presentó un error durante la covnersión, revise el formato del archivo cargado. " + str(e) 
-                errorTasks = errorTasks + 1
-                print('Error al convertir el archivo: ' + message)
-        pendingTask.status = FileStatus.PROCESSED if valid or "La conversión requerida ya había sido realizada, puede descargar el archivo" in message else FileStatus.ERROR
+                message = "La extensión del archivo a convertir no se encuentra dentro de las extensiones habilitadas"
 
-        session.commit()
-        try:
-            mailSender = MailSend()
-            user = session.query(User).filter(User.username == pendingTask.user).first()
-            subject = "Se ha terminado la conversión del archivo " + originFileName
-            message = "<br/> Hemos terminado la conversión del archivo con el siguiente resultado:<br/> <br/> <br/> " + message
-            mailSender.send_email(user.email, subject, message)
-        except Exception as e:
-            print("No se pudo enviar el correo"+str(e))
+            if not targetExtension in app_settings.EXTENSIONES_PERMITIDAS:
+                valid = False
+                message = "La extensión a la que desea convertir no se encuentra dentro de las extensiones habilitadas"
 
-        return str(validTasks) + " Ok, " + str(errorTasks) + " con error"           
+            if "." + targetExtension == extension:
+                valid = False
+                message = "La extensión a la que desea convertir es la misma del archivo orifinal y no se requiere realizar ninguna conversión"
+
+            if valid and not os.path.exists(originFilePath):
+                valid = False
+                message = "El archivo de origen no se encontró"
+
+            if valid and os.path.exists(targetFilePath):
+                valid = False
+                message = "La conversión requerida ya había sido realizada, puede descargar el archivo"
+                validTasks = validTasks + 1
+            print(message)
+            if valid:
+                try:
+                    GCPStorage.download_file(pendingTask.user + "/" + pendingTask.file, originFilePath)
+                    if extension == ".wav":
+                        input = AudioSegment.from_wav(originFilePath)
+                        input.export(targetFilePath, format=targetExtension)
+                    elif extension == ".mp3":
+                        input = AudioSegment.from_mp3(originFilePath)
+                        input.export(targetFilePath, format=targetExtension)
+                    elif extension == ".ogg":
+                        input = AudioSegment.from_ogg(originFilePath)
+                        input.export(targetFilePath, format=targetExtension)
+
+                    GCPStorage.upload_file(targetFilePath, pendingTask.user + "/" + fileName + "." + targetExtension)
+                    message = "El archivo ha sido descargado correctamente, podrá descargarlo con el nombre " + fileName + "." + targetExtension
+                    print(message)
+                    validTasks = validTasks + 1
+                except Exception as e:
+                    traceback.print_exc()
+                    valid = False
+                    message = "No se pudo realizar la conversión porque se presentó un error durante la covnersión, revise el formato del archivo cargado. " + str(e) 
+                    errorTasks = errorTasks + 1
+                    print('Error al convertir el archivo: ' + message)
+            pendingTask.status = FileStatus.PROCESSED if valid or "La conversión requerida ya había sido realizada, puede descargar el archivo" in message else FileStatus.ERROR
+
+            session.commit()
+            try:
+                mailSender = MailSend()
+                user = session.query(User).filter(User.username == pendingTask.user).first()
+                subject = "Se ha terminado la conversión del archivo " + originFileName
+                message = "<br/> Hemos terminado la conversión del archivo con el siguiente resultado:<br/> <br/> <br/> " + message
+                mailSender.send_email(user.email, subject, message)
+            except Exception as e:
+                print("No se pudo enviar el correo"+str(e))
+
+            return str(validTasks) + " Ok, " + str(errorTasks) + " con error"           
 
     
-    messageP.ack()
-
+        messageP.ack()
+    else:
+        print("Error al con el subscriptor")
 streaming_pull_future=subscriber.subscribe(sub_path,callback=convert_audio)
 
 with subscriber:
