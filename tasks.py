892190ba43011d72aa4db41f2dc96185154325a8
Brandon <b.gonzalezp@uniandes.edu.co>
2022-12-03 15:16:48 -0500

    ajustes

--- tasks.py
+++ tasks.py
@@ -36,7 +36,7 @@ def convert_audio(messageP):
             targetExtension = pendingTask.newExtension
             fileName, extension = os.path.splitext(originFileName)
             originFilePath = os.path.join(pendingTask.user, originFileName)
-            targetFilePath = os.path.join(app_settings.RUTA_REPOSITORIO, fileName + "." + targetExtension)
+            targetFilePath = os.path.join(app_settings.RUTA_REPOSITORIO,pendingTask.user, fileName + "." + targetExtension)
     
             if not targetExtension in app_settings.EXTENSIONES_PERMITIDAS:
                 valid = False
@@ -55,6 +55,8 @@ def convert_audio(messageP):
                 try:
                     print("/" + pendingTask.user + "/" + pendingTask.file + " > " + originFilePath)
                     print(targetFilePath)
+                    if(not os.path.exists(app_settings.RUTA_REPOSITORIO,pendingTask.user)):
+                        os.mkdir(app_settings.RUTA_REPOSITORIO,pendingTask.user)
                     tempFile = os.path.join(app_settings.RUTA_REPOSITORIO,pendingTask.user, pendingTask.file)
                     GCPStorage.download_file(pendingTask.user + "/" + pendingTask.file, tempFile)
                     if extension == ".wav":
@@ -68,6 +70,8 @@ def convert_audio(messageP):
                         input.export(targetFilePath, format=targetExtension)
                     
                     GCPStorage.upload_file(targetFilePath, pendingTask.user + "/" + fileName + "." + targetExtension)
+                    os.remove(os.path.join(app_settings.RUTA_REPOSITORIO,pendingTask.user, pendingTask.file))
+                    os.remove(os.path.join(targetFilePath))
                     message = "El archivo ha sido descargado correctamente, podrá descargarlo con el nombre " + fileName + "." + targetExtension
                     print(message+"\n")
                     validTasks = validTasks + 1
